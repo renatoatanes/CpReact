3f825119d5172c97f6b144d00dbc688fc9710074
renatoatanes <rm552029@fiap.com.br>
2023-09-05 13:43:14 -0300

    links+ rodas add

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home';
+import Produtos from './routes/Produtos';
+import Error from './routes/Error';
+import EditarProdutos from './routes/EditarProduto';
+
+const router = createBrowserRouter([
+  {path: "/" , element: <App/>, errorElement: <Error/>,
+    children:[
+      {path: "/" , element: <Home/>},
+      {path: "/produtos" , element: <Produtos/>},
+      {path: "/editar/produtos/:id" , element: <EditarProdutos/>},
+    ]}
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
